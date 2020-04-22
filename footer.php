2c212927b4251c2335981454e3e3784d21e53feb
Tommi Boom <tommiboom@protonmail.com>
2020-04-22 13:51:54 +0200

    footer alyout and license

--- footer.php
+++ footer.php
@@ -1,1 +1,19 @@
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br />Everything in this website, including its source code, is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+<footer>
+	<div class="row">
+		<div class="one-third column">
+			<p>prova</p>
+		</div>
+		<div class="one-third column">
+			<p>prova</p>
+		</div>
+		<div class="one-third column">
+			<p>prova</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="one column" id="license">
+			<p>Everything in this website, including its source code, is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.</p>
+			<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" src="cc-by-nc-sa-little.svg" /></a>
+		</div>
+	</div>
+</footer>
