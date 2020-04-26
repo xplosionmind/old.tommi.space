703a77e2516909e9669717067f67a6e094c15f32
Tommi Boom <tommiboom@protonmail.com>
2020-04-26 11:40:21 +0200

    social logos and transitions added

--- footer.php
+++ footer.php
@@ -6,7 +6,12 @@
 				<input type="submit" id="submit" value="GO" />
 		</div>
 		<div class="one-fifth column">
-			<p>prova</p>
+			<div id="social-box">
+				<a href="https://www.flickr.com/tommiboom" alt="my Flickr profile"><img class="social" alt="Flickr" src="social-logos/flickr.svg" /></a>
+				<a href="https://keybase.io/xplosionmind" alt="my Keybase profile"><img class="social" alt="Keybase" src="social-logos/keybase.svg" /></a>
+				<a href="https://github.com/xplosionmind" alt="my GitHub profile"><img class="social" alt="GitHub" src="social-logos/github.svg" /></a>
+				<a href="!#" alt="my Mastodon profile"><img class="social" alt="Mastodon" src="social-logos/mastodon.svg" /></a>
+			</div>
 		</div>
 		<div class="one-fifth column">
 			<p>prova</p>
@@ -18,7 +23,7 @@
 	<div class="row">
 		<div class="one column" id="license">
 			<p>Everything in this website, including its source code, is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.</p>
-			<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" src="cc-by-nc-sa-little.svg" /></a>
+			<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img id="CClogo" alt="Creative Commons License" src="cc-by-nc-sa-little.svg" /></a>
 		</div>
 	</div>
 </footer>
